eb5c0db43d80a2e808d158d900342dd85abcc675
Aleksandr Pechak <alpechak@gmail.com>
2024-05-29 16:46:04 +0300

    update

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,5 +1,14 @@
+import fs from 'fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const getAllContacts = async () => {};
+export const getAllContacts = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf8');
+    const contacts = JSON.parse(data);
+    return contacts;
+  } catch (err) {
+    console.log('Error while getting contacts: ', err);
+  }
+};
 
 console.log(await getAllContacts());
